495e53d9d6cb8834f1fddabf9063b750bd8d6da7
Fizzy <21183059+Fizzyhex@users.noreply.github.com>
2026-09-06 17:06:33 +0100

    chore: cleanup website title

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "👥 BONELAB Unofficial Docs (ARCHIVE)",
+    pageTitle: "BONELAB Unofficial Docs",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
